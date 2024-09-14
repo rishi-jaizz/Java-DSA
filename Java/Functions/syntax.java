2be915e67fc205413ab74fbbdcf9b21be59b2b77
rishi-jaizz <rishijaiswal888@gmail.com>
2024-09-15 03:16:53 +0530

    function in java

---
+++ Java/Functions/syntax.java
@@ -0,0 +1,11 @@
+package Java.Functions;
+
+public class syntax {
+    public static void printHelloWorld(){ // Block of code
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        printHelloWorld(); // function call
+    }
+    
+}
